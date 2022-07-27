da15d9efb1dae89431103085faa2ded0839c6c88
Chris Lyman <christopher.lyman0@gmail.com>
2022-07-27 10:53:12 -0400

    add intersts array

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -24,4 +24,12 @@ RSpec.describe Patron do
       expect(patron_1.spending_money).to eq(20)
     end
   end
+
+  describe 'interests' do
+    it 'has an empty array of interests' do
+      patron_1 = Patron.new("Bob", 20)
+
+      expect(patron_1.interests).to eq([])
+    end
+  end
 end
