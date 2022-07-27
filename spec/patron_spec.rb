f656a73eee8cb28d3a9749448de97c1df7b15724
Chris Lyman <christopher.lyman0@gmail.com>
2022-07-27 11:00:13 -0400

    add patron add_interests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -32,4 +32,15 @@ RSpec.describe Patron do
       expect(patron_1.interests).to eq([])
     end
   end
+
+  describe '#add_interests' do
+    it 'has an array of interests' do
+      patron_1 = Patron.new("Bob", 20)
+
+      patron_1.add_interests("Dead Sea Scrolls")
+      patron_1.add_interests("Gems and Minerals")
+
+      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end 
+  end
 end
