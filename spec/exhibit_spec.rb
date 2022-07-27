b3445174a8ae691d52e79ccf1eb9df6a7f59396e
Chris Lyman <christopher.lyman0@gmail.com>
2022-07-27 10:20:43 -0400

    add initialize

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,11 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  describe '#initialize' do
+    it 'exists' do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+      expect(exhibit).to be_instance_of(Exhibit)
+  end
+end
