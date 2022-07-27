d91516cd4df0de7d423a2c95f26ab68e3ae94a7c
Chris Lyman <christopher.lyman0@gmail.com>
2022-07-27 11:12:21 -0400

    add museum name

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -3,11 +3,19 @@ require './lib/patron'
 require './lib/exhibit'
 
 RSpec.describe Museum do
-  descrbe '#initialize' do
+  describe '#initialize' do
     it "exsits" do
       dmns = Museum.new("Denver Museum of Nature and Science")
 
        expect(dmns).to be_instance_of(Museum)
+    end
+  end
+
+  describe '@name' do
+    it 'has a name' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+
+      expect(dmns.name).to eq("Denver Museum of Nature and Science")
     end
   end
 end
